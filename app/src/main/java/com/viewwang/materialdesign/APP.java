5111988ec3b16aa524a37e6a9e8f4dcf2d483209
wjx367 <646432904@qq.com>
2017-02-27 16:53:38 +0800

    app
    add app

---
+++ app/src/main/java/com/viewwang/materialdesign/APP.java
@@ -0,0 +1,20 @@
+package com.viewwang.materialdesign;
+
+import android.app.Application;
+
+/**
+ * Title:APP
+ * Package:com.viewwang.materialdesign
+ * Description:TODO
+ * Author: wjx@tomcat360.com
+ * Date: 2017/2/27 0027
+ * Version: V1.0.0
+ * 版本号修改日期修改人修改内容
+ */
+
+public class APP extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
